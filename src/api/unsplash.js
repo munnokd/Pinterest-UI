57a97e64f37c85945eb26d90359b9af5cff4d2bf
Kalp Prajapati <81804004+munnokd@users.noreply.github.com>
2021-07-03 08:41:44 +0530

    Add files via upload

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,1 +1,8 @@
+import axios from 'axios'
 
+export default axios.create({
+    baseUrl:'https://api.unsplash.com/',
+    headers:{
+        Authorization:"Client-ID _Hdvct4p3awZ9lz1DPXjnfk9_3BnAnaUAQ-VS8mWO-c"
+    }
+})
