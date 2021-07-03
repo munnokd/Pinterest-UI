0d35c8b10c5ab6fcbcba2df9a0e136466a1fd0a6
Kalp Prajapati <81804004+munnokd@users.noreply.github.com>
2021-07-03 08:43:43 +0530

    Add files via upload

--- src/MainBoard/Main.js
+++ src/MainBoard/Main.js
@@ -1,1 +1,20 @@
+import React from 'react'
+import './Main.css'
+import Pin from './Pin'
 
+const Main = (props) => {
+    let {pins}=props;
+
+    return (
+        <div className="main">
+            <div className="main__container">
+                {pins.map((pin,index)=>{
+                    let {urls}=pin;
+                    return <Pin key={index} urls={urls}/>
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Main
---
+++ src/MainBoard/Pin.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Pin.css'
+
+const Pin = (props) => {
+    let {urls}=props;
+    return (
+        <div className="pin">
+            <div className="pin__container">
+                <img src={urls.regular}
+                alt="pin"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Pin
